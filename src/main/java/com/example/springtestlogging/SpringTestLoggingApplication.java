e88a0a4314c76dc2875db13dcffc5f5cb200e566
Aarish <aarishm767@gmail.com>
2023-01-31 20:19:12 +0530

    Initial Commit

---
+++ src/main/java/com/example/springtestlogging/SpringTestLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtestlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtestlogging/SpringTestLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtestlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
